16964b4a25b4f8685840f27b67aead9830fab54b
ming <736396122@qq.com>
2018-08-16 16:07:13 +0800

    solved 035 searchInsert

--- twosum/main.cpp
+++ twosum/main.cpp
@@ -802,6 +802,28 @@ public:
         }
         return ans[n];
     }
+    int searchInsert(vector<int>& nums, int target) {
+        int l=0,r=nums.size();
+        if(target<nums[l])
+            return l;
+        if(target>nums[r-1])
+            return r;
+        while(l<=r)
+        {
+            int mid=(l+r)/2;
+            if(target==nums[mid])
+                return mid;
+            else if(target<nums[mid])
+            {
+                r=mid-1;
+            }
+            else if(target>nums[mid])
+            {
+                l=mid+1;
+            }
+        }
+        return l;
+    }
 };
 void print(ListNode *l)
 {
@@ -926,6 +948,13 @@ int main()
 //test.push_back(3);
 //test.push_back(3);
 //cout<<ts->removeElement(test,3)<<"\n";
+vector<int> test;
+test.clear();
+test.push_back(1);
+test.push_back(3);
+test.push_back(5);
+test.push_back(6);
+
     string s,p;
     int n;
     while(cin>>n)
@@ -937,7 +966,8 @@ int main()
 //cout<<ts->intToRoman(n)<<"\n";
 // cout<<ts->romanToInt(s)<<"\n";
 //cout<<ts->isValid(s)<<"\n";
-cout<<ts->countAndSay(n)<<"\n";
+//cout<<ts->countAndSay(n)<<"\n";
+cout<<ts->searchInsert(test,n)<<"\n";
     }
     return 0;
 }
